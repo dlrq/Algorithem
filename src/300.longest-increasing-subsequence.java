29260ed16353c22250e525155d1d6479a687991a
dlrq <liruquan.dlrq@alibaba-inc.com>
2021-06-02 11:02:17 +0800

    longest increasing subsequence best solution

---
+++ src/300.longest-increasing-subsequence.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=300 lang=java
+ *
+ * [300] Longest Increasing Subsequence
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums.length == 1){
+            return 1;
+        }
+
+        int[] res = new int[nums.length];
+        res[0] = Integer.MAX_VALUE;
+        int low = 0;
+        int high = 1;
+        for(int num : nums){
+            int pos = Arrays.binarySearch(res, low, high, num);
+            if(pos < 0){
+                pos = -pos - 1;
+            }
+            res[pos] = num;
+            if(pos == high){
+                high++;
+            }
+        }
+        return high;
+    }
+}
+// @lc code=end
+
--- src/ContainerWithMostWater.java
+++ src/ContainerWithMostWater.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.Map;
-import java.util.TreeMap;
+
 
 public class ContainerWithMostWater {
     public static void main(String[] args) {
