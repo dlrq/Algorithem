05606a738c686d6afd2ac9146fa6aca75cf4bce0
Liruquan Duan <dlrqtc@gmail.com>
2021-06-12 23:58:02 +0800

    convert-sorted-array-to-binary-search-tree solution

--- src/108.convert-sorted-array-to-binary-search-tree.java
+++ src/108.convert-sorted-array-to-binary-search-tree.java
@@ -22,7 +22,18 @@
  */
 class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
-        
+        if(nums == null){
+            return null;
+        }
+        return helper(0, nums.length - 1, nums);
+    }
+
+    public TreeNode helper(int low, int high, int[] nums){
+        if(low > high) {
+            return null;
+        }
+        int mid = (low + high) / 2;
+        return new TreeNode(nums[mid], helper(low, mid - 1, nums), helper(mid + 1, high, nums));
     }
 }
 // @lc code=end
